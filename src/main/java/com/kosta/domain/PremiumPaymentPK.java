892d8971e7f982af455689ba23edd087ce183bc0
existmaster <existmaster@gmail.com>
2018-05-30 17:57:17 +0900

    Rename PremiumPaymentID to PK

--- src/main/java/com/kosta/domain/PremiumPaymentPK.java
+++ src/main/java/com/kosta/domain/PremiumPaymentPK.java
@@ -2,11 +2,14 @@ package com.kosta.domain;
 
 import lombok.Data;
 
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
-public class PremiumPaymentID implements Serializable {
+public class PremiumPaymentPK implements Serializable {
     InsurancePolicy insurancePolicy;
     private Date paymentDate;
 }
